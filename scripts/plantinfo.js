a348d61524484390c09f0cfeae47c8856a98a332
FUVESZTAR <fuvesztar@gmail.com>
2026-08-30 21:59:25 +0200

    Update plantinfo.js

--- scripts/plantinfo.js
+++ scripts/plantinfo.js
@@ -1242,7 +1242,7 @@ document.querySelector('#back-button').addEventListener('click', () => {
     console.log('imageset input LatinName: ', plant.LatinName, ' , Variety: ',  plant.Name_Variety);
     console.log('imageset input LatinName_fix: ', plant.LatinName_fix, ' , Variety_fix: ',  plant.Name_Variety_fix);
     console.log('isPicture:',plant.isPicture);
-    loadPlantImageSet(plant.LatinName_fix,plant.Name_Variety_fix,plant.isPicture));
+    loadPlantImageSet(plant.LatinName_fix,plant.Name_Variety_fix,plant.isPicture);
 
     // Translate
     applyTranslations();
